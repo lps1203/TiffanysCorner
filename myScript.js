c04f70edbc2922542be68e761c1591e8ffbb7741
Yoonwhan Ahn <lps1203@hotmail.com>
2016-09-18 07:07:09 +0900

    check persistence of localStroage on cell phone

--- myScript.js
+++ myScript.js
@@ -1,7 +1,6 @@
 var show = document.getElementById('show');
-localStorage.setItem('age', '125');
-localStorage.setItem('color', 'black');
+
 var age = localStorage.getItem('age');
 var color = localStorage.getItem('color');
 
-show.innerHTML = age + " ***" + color;
+show.innerHTML = age + " ------------" + color;
