6f08af2c561de52b478263cb40258451f463e168
Yoonwhan Ahn <lps1203@hotmail.com>
2016-09-18 07:04:24 +0900

    trying localStorage on a cell phone

--- myScript.js
+++ myScript.js
@@ -1,4 +1,6 @@
 var show = document.getElementById('show');
+localStorage.setItem('age', '125');
+localStorage.setItem('color', 'black');
 var age = localStorage.getItem('age');
 var color = localStorage.getItem('color');
 
