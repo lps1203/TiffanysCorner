87a70287afab65caba3ba51e9457a68aa3c2480f
Yoonwhan Ahn <lps1203@hotmail.com>
2016-09-17 23:46:12 +0900

    testing  localStorage's persistence

--- myScript.js
+++ myScript.js
@@ -1,7 +1,5 @@
 var show = document.getElementById('show');
-localStorage.setItem('age', '12');
-localStorage.setItem('color', 'blue');
 var age = localStorage.getItem('age');
 var color = localStorage.getItem('color');
 
-show.innerHTML = age + " " + color;
+show.innerHTML = age + " ***" + color;
