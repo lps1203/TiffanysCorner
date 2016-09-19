b3ddab1496d520d1f7e88230ea09d95d4df75ae6
Yoonwhan Ahn <lps1203@hotmail.com>
2016-09-19 16:33:24 +0900

    log system 100% totally completed!!!

--- loginScript.js
+++ loginScript.js
@@ -5,6 +5,8 @@ var loginBtn = document.getElementById('login-btn');
 
 var loginSuccess = '<div class="container"><div class="row"><div class="col-sm-3"></div><div class="col-sm-6"><div class="alert alert-success"><strong>Success!</strong> Redirecting to the Homepage in 3 sec...</div><br></div><div class="col-sm-3"></div></div></div>';
 var loginFail = '<div class="container"><div class="row"><div class="col-sm-3"></div><div class="col-sm-6"><div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>Sorry!</strong> Username and/or Password is not correct. Try again.</div><br></div><div class="col-sm-3"></div></div></div>';
+var loginWarn = '<div class="container"><div class="row"><div class="col-sm-3"></div><div class="col-sm-6"><div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>Warning!</strong> Current user needs to log out first. If you proceed to log in, the current user will be automatically logged out and all session data will be lost.</div><br></div><div class="col-sm-3"></div></div></div>';
+
 
 function gotoLogin() {
     window.location.href = 'login.html';
@@ -32,5 +34,29 @@ loginBtn.addEventListener('click', function() {
     }
 }, false);
 
+function checkLoggedIn() {
+    var userEl = document.getElementById('loggedInUsername');
+    var loggedIn = sessionStorage.getItem('loggedIn');
+    if(loggedIn) {
+        loginResult.innerHTML = loginWarn;
+        userEl.textContent = sessionStorage.getItem('username');
+        // remove login menu
+        var logEl = document.getElementById('login');
+        var parentLog = logEl.parentNode;
+        parentLog.removeChild(logEl);
+        // add logout menu
+        var newLi = document.createElement('li');
+        newLi.id = 'logout';
+        var newA = document.createElement('a');
+        newA.href = 'logout.html';
+        var newSpan = document.createElement('span');
+        newSpan.className = 'glyphicon glyphicon-log-out';
+        var newText = document.createTextNode('Logout');
+        newA.appendChild(newSpan);
+        newA.appendChild(newText);
+        newLi.appendChild(newA);
+        parentLog.appendChild(newLi);
+    }
+}
 
 
--- signupScript.js
+++ signupScript.js
@@ -4,6 +4,7 @@ var signupBtn = document.getElementById('signup-btn');
 
 var signupSuccess = '<div class="container"><div class="row"><div class="col-sm-3"></div><div class="col-sm-6"><div class="alert alert-success"><strong>Success!</strong> Redirecting to the login page in 5 sec...</div><br></div><div class="col-sm-3"></div></div></div>';
 var signupFail = '<div class="container"><div class="row"><div class="col-sm-3"></div><div class="col-sm-6"><div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>Sorry!</strong> Username is already taken. Try again with a different username.</div><br></div><div class="col-sm-3"></div></div></div>';
+var signupWarn = '<div class="container"><div class="row"><div class="col-sm-3"></div><div class="col-sm-6"><div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>Warning!</strong> Current user needs to log out first. If you proceed to sign up, the current user will be automatically logged out and all session data will be lost.</div><br></div><div class="col-sm-3"></div></div></div>';
 
 
 function gotoLogin() {
@@ -29,3 +30,28 @@ signupBtn.addEventListener('click', function() {
         setTimeout(gotoLogin, 5000);
     }
 }, false);
+
+function checkLoggedIn() {
+    var userEl = document.getElementById('loggedInUsername');
+    var loggedIn = sessionStorage.getItem('loggedIn');
+    if(loggedIn) {
+        signupResult.innerHTML = signupWarn;
+        userEl.textContent = sessionStorage.getItem('username');
+        // remove login menu
+        var logEl = document.getElementById('login');
+        var parentLog = logEl.parentNode;
+        parentLog.removeChild(logEl);
+        // add logout menu
+        var newLi = document.createElement('li');
+        newLi.id = 'logout';
+        var newA = document.createElement('a');
+        newA.href = 'logout.html';
+        var newSpan = document.createElement('span');
+        newSpan.className = 'glyphicon glyphicon-log-out';
+        var newText = document.createTextNode('Logout');
+        newA.appendChild(newSpan);
+        newA.appendChild(newText);
+        newLi.appendChild(newA);
+        parentLog.appendChild(newLi);
+    }
+}
