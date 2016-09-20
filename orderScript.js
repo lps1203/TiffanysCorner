db8172ff0a8b3deb316bb385457f635ddf8c9b43
Yoonwhan Ahn <lps1203@hotmail.com>
2016-09-20 17:56:07 +0900

    added order summary part. hopely final version

--- orderScript.js
+++ orderScript.js
@@ -30,6 +30,45 @@ function clearSession() {
     sessionStorage.clear();
 }
 
+
+var orderBtn1 = document.getElementById('order-btn1');
+var orderSum = document.getElementById('order-summary');
+var prices = ['      ', '$59.99', '$89.99', '$89.99', '$9.99',
+                      '$14.99', '$9.99',  '$9.99',  '$9.99',
+                      '$25.99', '$75.99', '$15.99', '$29.99',
+                      '$149.99', '$199.99', '$99.99', '$159.99'];
+var num = [0,  1,  3,  4,  5,
+             6,  8,  9, 10,
+            11, 12, 13, 14,
+            15, 16];
+
+var tabRows = '<table class="table"><thead><tr><th>Item No.</th><th>Price</th></tr></thead><tbody>';
+var totalPrice = 0;
+
+orderBtn1.addEventListener('click', function() {
+    for(var i = 1; i <= 14; i++) {
+        var j = num[i];
+        if(document.getElementById('c' + j).checked) {
+            tabRows += '<tr><td>No. ' + j + '</td><td>' + prices[j] + '</td></tr>';
+            totalPrice += Math.round(Number(prices[j].replace('$', ''))*100);
+        }
+    }
+    tabRows += '<tr class="info" style="font-weight: bold"><td>Total Price</td><td>$' + totalPrice / 100 + '</td></tr></tbody></table>';
+    orderSum.innerHTML = tabRows;
+
+
+    tabRows = '<table class="table"><thead><tr><th>Item No.</th><th>Price</th></tr></thead><tbody>';
+    totalPrice = 0;
+ }, false);
+
+    
+
+
+
+
+
+
+
 var contactBtn = document.getElementById('contact-btn');
 
 contactBtn.addEventListener('click', function() {
